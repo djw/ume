e19310f4ba69184eaade65432ced000d2061d9bc
Dan Wilson <work@danjwilson.co.uk>
2023-02-25 11:51:05 +0000

    Simple MVP for assuming a role

--- ume.go
+++ ume.go
@@ -3,11 +3,14 @@ package main
 import (
 	"context"
 	"flag"
+	"fmt"
 	"log"
 	"os"
 	"strings"
 
+	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/sts"
 	"github.com/olekukonko/tablewriter"
 	"gopkg.in/ini.v1"
 )
@@ -27,19 +30,32 @@ func listConfigNames() []string {
 	return profiles
 }
 
-func getConfigs() []config.SharedConfig {
+func getConfig(profile string) aws.Config {
+	config, err := config.LoadDefaultConfig(context.TODO(), config.WithSharedConfigProfile(profile))
+	if err != nil {
+		log.Fatal(err)
+	}
+	return config
+}
+
+func getSharedConfig(profile string) config.SharedConfig {
+	sharedConfig, err := config.LoadSharedConfigProfile(context.TODO(), profile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return sharedConfig
+}
+
+func getSharedConfigs() []config.SharedConfig {
 	var configs []config.SharedConfig
 	for _, profile := range listConfigNames() {
-		sharedConfig, err := config.LoadSharedConfigProfile(context.TODO(), profile)
-		if err != nil {
-			log.Fatal(err)
-		}
-		configs = append(configs, sharedConfig)
+		configs = append(configs, getSharedConfig(profile))
 	}
 	return configs
 }
 
-func prettyPrintConfigs() {
+func prettyPrintSharedConfigs() {
+	fmt.Println("Listing...")
 	table := tablewriter.NewWriter(os.Stdout)
 	table.SetBorder(false)
 	table.SetColumnSeparator("")
@@ -48,7 +64,7 @@ func prettyPrintConfigs() {
 	table.SetHeaderAlignment(tablewriter.ALIGN_LEFT)
 	table.SetAlignment(tablewriter.ALIGN_LEFT)
 	table.SetHeader([]string{"PROFILE", "TYPE", "SOURCE", "MFA?", "REGION", "ACCOUNT"})
-	for _, c := range getConfigs() {
+	for _, c := range getSharedConfigs() {
 		var profile_type string
 		var source_profile string
 		if c.RoleARN != "" {
@@ -77,11 +93,46 @@ func prettyPrintConfigs() {
 	table.Render()
 }
 
+func exportToWrapper(roleSession sts.AssumeRoleOutput, profile string) {
+	fmt.Printf("%s %s %s %s %s %s %s %s\n", "Awsume", *roleSession.Credentials.AccessKeyId, *roleSession.Credentials.SecretAccessKey, *roleSession.Credentials.SessionToken, "eu-west-1", "None", profile, roleSession.Credentials.Expiration.Format("2006-01-02T15:04:05"))
+}
+
 func main() {
 	listProfiles := flag.Bool("l", false, "List profiles")
 	flag.Parse()
 
 	if *listProfiles {
-		prettyPrintConfigs()
+		prettyPrintSharedConfigs()
+	} else if flag.NArg() == 1 {
+		profileName := flag.Arg(0)
+
+		c := getSharedConfig(profileName)
+
+		targetProfileName := profileName
+		targetProfile := c
+
+		if targetProfile.SourceProfileName != "" {
+			targetProfileName = targetProfile.SourceProfileName
+			targetProfile = getSharedConfig(targetProfileName)
+		}
+
+		config := getConfig(targetProfileName)
+		client := sts.NewFromConfig(config)
+
+		sessionName := "ume-session"
+		roleSession, err := client.AssumeRole(context.TODO(), &sts.AssumeRoleInput{
+			RoleArn:         &c.RoleARN,
+			RoleSessionName: &sessionName,
+		})
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		greenColour := "\033[32m"
+		resetColour := "\033[0m"
+		fmt.Fprintf(os.Stderr, "%s[%s] Role credentials will expire %s%s\n", greenColour, profileName, *roleSession.Credentials.Expiration, resetColour)
+		exportToWrapper(*roleSession, profileName)
 	}
+
 }
